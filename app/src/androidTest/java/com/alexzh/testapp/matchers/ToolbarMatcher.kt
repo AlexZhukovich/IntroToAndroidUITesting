d6b538df91edba99d6e8b0556c6732313472c735
Alex Zhukovich <zhukovich.a@gmail.com>
2019-08-13 13:26:35 +0200

    Added files for writing test cases

---
+++ app/src/androidTest/java/com/alexzh/testapp/matchers/ToolbarMatcher.kt
@@ -0,0 +1,22 @@
+package com.alexzh.testapp.matchers
+
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import androidx.test.espresso.matcher.BoundedMatcher
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+
+object ToolbarMatcher {
+
+    fun withToolbarTitle(textMatcher: Matcher<String>): Matcher<View> {
+        return object : BoundedMatcher<View, Toolbar>(Toolbar::class.java) {
+            override fun describeTo(description: Description?) {
+
+            }
+
+            override fun matchesSafely(toolbar: Toolbar?): Boolean {
+                return false
+            }
+        }
+    }
+}
