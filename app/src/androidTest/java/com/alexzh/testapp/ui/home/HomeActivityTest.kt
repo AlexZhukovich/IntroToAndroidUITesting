3e04511516a414f814918d035b838107ae479f7a
Alex Zhukovich <zhukovich.a@gmail.com>
2019-09-22 22:43:02 +0200

    Added comments for test class and each test case

--- app/src/androidTest/java/com/alexzh/testapp/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/alexzh/testapp/ui/home/HomeActivityTest.kt
@@ -1,67 +1,165 @@
 package com.alexzh.testapp.ui.home
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.IntentMatchers
-import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
-import com.alexzh.testapp.R
-import com.alexzh.testapp.actions.RecyclerViewActions.clickByChildViewWithId
-import com.alexzh.testapp.data.DummyData
-import com.alexzh.testapp.matchers.RecyclerViewMatchers.atPosition
-import com.alexzh.testapp.matchers.RecyclerViewMatchers.withItemCount
-import com.alexzh.testapp.matchers.ToolbarMatcher.withToolbarTitle
-import com.alexzh.testapp.ui.home.adapter.TaskViewHolder
-import com.alexzh.testapp.ui.settings.SettingsActivity
-import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.CoreMatchers.not
-import org.junit.Assert
 import org.junit.Assert.fail
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+/**
+ * Test suite for checking the HomeActivity class from a UI perspective.
+ *
+ * Note: This class can be run without any pre-requirements, as in real project.
+ * The HomeActivity class doesn't verify if User logged in because the main idea of the project
+ * is learning basics of Espresso framework. You can check I'mBarista project for more complex
+ * practices and test cases.
+ */
 @RunWith(AndroidJUnit4::class)
 class HomeActivityTest {
 
+    @Rule
+    @JvmField
+    val activity = ActivityTestRule(HomeActivity::class.java)
+
+    /**
+     * Test case:
+     *  - Toolbar should have a text "TestApp"
+     *
+     * Notes:
+     *  - the "Toolbar" view has ID: toolbar (R.id.toolbar)
+     *  - the "TestApp" string can be found in strings.xml file as app_name const (R.string.app_name)
+     *
+     * Hint(s):
+     *  - the toolbar title can be checked with ToolbarMatcher#withToolbarTitle ViewMatcher
+     *  (can be not implemented yet).
+     */
     @Test
     fun shouldToolbarContainsTextAppTitle() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  - RecyclerView should have 15 items
+     *
+     * Notes:
+     *  - the "RecyclerView" has ID: recyclerView (R.id.recyclerView)
+     *  - the "RecyclerView" shows items created in DummyData#getTasks() method
+     *
+     * Hint(s):
+     *  - the number of items in RecyclerView can be checked with RecyclerViewMatchers#withItemCount
+     *  ViewMatcher (can be not implemented yet).
+     */
     @Test
     fun shouldRecyclerViewHas15Items() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  - HomeActivity should have options menu
+     *  - SettingsActivity should be opened after pressing on "Settings" in options menu
+     *
+     * Notes:
+     *  - the "Settings" text can be found in strings.xml file as settings_action const
+     *  (R.string.settings_action)
+     *
+     * Hint(s):
+     *  - Intent.intended(IntentMatcher) allows us to verify that Activity was opened.
+     *  The Intents component should be initialized before with the Intents#init() method
+     *  and released when it's not needed with the Intents#release() method.
+     *  - the Espresso#openActionBarOverflowOrOptionsMenu method allows us to open options menu.
+     */
     @Test
     fun shouldOpenSettingsScreenWhenSettingsItemClicked() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  - the task title should be displayed after click on item from RecyclerView
+     *
+     * Notes:
+     *  - the "RecyclerView" has ID: recyclerView (R.id.recyclerView)
+     *  - the "RecyclerView" shows items created in DummyData#getTasks() method
+     *
+     * Hint(s):
+     *  - the action on item can be made with RecyclerViewActions#actionOnItem method.
+     */
     @Test
     fun shouldBeDisplayedTaskTitleWhenClickOnTask() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  - the "The {task title} task is favourite" or "The {task title} task is not favourite" text
+     *  should be displayed in Snackbar after click on favourite icon of the item, depends on
+     *  the status of the item.
+     *
+     * Notes:
+     *  - the data displayed in the RecyclerView can found in DummyData#getTasks() method.
+     *  - the "The {task title} task is favourite" string can be found in string.xml file as
+     *  formatted_favourite_task const (R.string.formatted_favourite_task).
+     *  - the "The {task title} task is not favourite" string can be found in string.xml file as
+     *  formatted_not_favourite_task const (R.string.formatted_not_favourite_task).
+     *  - the Snackbar component displays text which can be found in view with ID: snackbar_text
+     *  (com.google.android.material.R.id.snackbar_text).
+     *
+     * Hint(s):
+     *  - the click on child item, like favourite icon can be made with
+     *  RecyclerViewActions#clickByChildViewWithId ViewAction (can be not implemented yet).
+     */
     @Test
     fun shouldBeDisplayTaskInfoWhenClickedOnFavouriteIcon() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  - the first task should be favourite. We can verify it by verification ViewHolder of the
+     *  task. Each item has two descendants: title and icon.
+     *
+     * Notes:
+     *  - the view with ID: favouriteImageView (R.id.favouriteImageView) displays the drawable
+     *  with ID: ic_favorite_black_24dp (R.drawable.ic_favorite_black_24dp) when task is favourite.
+     *  - the data displayed in the RecyclerView can found in DummyData#getTasks() method.
+     *  - comparing rendered image with image from drawable file is very inefficient because image
+     *  can be resized or rendered from vector image. As an alternative we can use the TAG attribute
+     *  and set TAG as a file name of used resource.
+     *  - the "RecyclerView" has ID: recyclerView (R.id.recyclerView).
+     *
+     * Hint(s):
+     *  - the checking descendant property can done with RecyclerViewMatchers#atPosition ViewMatcher
+     *  (can be not implemented yet).
+     */
     @Test
     fun shouldFirstTestItemIsFavourite() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  - the lat task should be not favourite. We can verify it by verification ViewHolder of the
+     *  task. Each item has two descendants: title and icon.
+     *
+     * Note:
+     *  - the view with ID: favouriteImageView (R.id.favouriteImageView) displays the drawable
+     *  with ID: ic_favorite_border_black_24dp (R.drawable.ic_favorite_border_black_24dp) when task
+     *  is not favourite.
+     *  - the data displayed in the RecyclerView can found in DummyData#getTasks() method.
+     *  - comparing rendered image with image from drawable file is very inefficient because image
+     *  can be resized or rendered from vector image. As an alternative we can use the TAG attribute
+     *  and set TAG as a file name of used resource.
+     *  - the "RecyclerView" has ID: recyclerView (R.id.recyclerView).
+     *
+     * Hint(s):
+     *  - the checking descendant property can done with RecyclerViewMatchers#atPosition ViewMatcher
+     * (can be not implemented yet).
+     */
     @Test
-    fun shouldTenthTestItemIsNotFavourite() {
-        fail()
+    fun shouldLastTestItemIsNotFavourite() {
+       fail()
     }
 }
