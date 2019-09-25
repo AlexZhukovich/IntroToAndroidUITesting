515a4c4b73ef2137ce3c01b15af0aa2fe07080e0
Alex Zhukovich <zhukovich.a@gmail.com>
2019-09-25 23:25:01 +0200

    Added comment for the class a comment templates for the add test cases in LoginActivityTest class

--- app/src/androidTest/java/com/alexzh/testapp/ui/login/LoginActivityTest.kt
+++ app/src/androidTest/java/com/alexzh/testapp/ui/login/LoginActivityTest.kt
@@ -1,63 +1,184 @@
 package com.alexzh.testapp.ui.login
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import org.junit.Assert.fail
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+/**
+ * Test suite for checking the LoginActivity class from UI perspective.
+ *
+ * Note: This class can be run without any pre-requirements.
+ */
 @RunWith(AndroidJUnit4::class)
 class LoginActivityTest {
 
+    @Rule
+    @JvmField
+    val activity = ActivityTestRule(LoginActivity::class.java)
+
+    /**
+     * Test case:
+     *  -
+     *
+     * Notes:
+     *  -
+     *
+     * Hint(s):
+     *  -
+     */
     @Test
     fun shouldBeEnabledAllViews() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  -
+     *
+     * Notes:
+     *  -
+     *
+     * Hint(s):
+     *  -
+     */
     @Test
     fun shouldEmailAndPasswordFieldsDisplayHint() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  -
+     *
+     * Notes:
+     *  -
+     *
+     * Hint(s):
+     *  -
+     */
     @Test
     fun shouldDisplayErrorWhenEmailAndPasswordAreBlank() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  -
+     *
+     * Notes:
+     *  -
+     *
+     * Hint(s):
+     *  -
+     */
     @Test
     fun shouldDisplayErrorWhenEmailIsBlank() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  -
+     *
+     * Notes:
+     *  -
+     *
+     * Hint(s):
+     *  -
+     */
     @Test
     fun shouldDisplayErrorWhenPasswordIsBlank() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  -
+     *
+     * Notes:
+     *  -
+     *
+     * Hint(s):
+     *  -
+     */
     @Test
     fun shouldDisplayErrorWhenEmailTextIsNotEmail() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  -
+     *
+     * Notes:
+     *  -
+     *
+     * Hint(s):
+     *  -
+     */
     @Test
     fun shouldDisplayErrorWhenEmailAndPasswordAreIncorrect() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  -
+     *
+     * Notes:
+     *  -
+     *
+     * Hint(s):
+     *  -
+     */
     @Test
     fun shouldDisplayErrorWhenEmailIsIncorrect() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  -
+     *
+     * Notes:
+     *  -
+     *
+     * Hint(s):
+     *  -
+     */
     @Test
     fun shouldDisplayErrorWhenPasswordIsIncorrect() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  -
+     *
+     * Notes:
+     *  -
+     *
+     * Hint(s):
+     *  -
+     */
     @Test
     fun shouldDisplayErrorWhenAgreeCheckboxIsNotChecked() {
         fail()
     }
 
+    /**
+     * Test case:
+     *  -
+     *
+     * Notes:
+     *  -
+     *
+     * Hint(s):
+     *  -
+     */
     @Test
     fun shouldOpenHomeScreenWhenEmailAndPasswordAreCorrect() {
         fail()
