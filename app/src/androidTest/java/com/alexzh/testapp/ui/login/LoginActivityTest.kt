77e5f864a204f0d18aa35656c17a2340f90b10da
Alex Zhukovich <zhukovich.a@gmail.com>
2019-09-27 00:11:07 +0200

    Added comments for test cases in LoginActivity class

--- app/src/androidTest/java/com/alexzh/testapp/ui/login/LoginActivityTest.kt
+++ app/src/androidTest/java/com/alexzh/testapp/ui/login/LoginActivityTest.kt
@@ -8,7 +8,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 /**
- * Test suite for checking the LoginActivity class from UI perspective.
+ * Test suite for checking the LoginActivity class from a UI perspective.
  *
  * Note: This class can be run without any pre-requirements.
  */
@@ -21,13 +21,16 @@ class LoginActivityTest {
 
     /**
      * Test case:
-     *  -
+     *  - the "Email Input" view is enabled
+     *  - the "Password Input" view is enabled
+     *  - the "I agree" checkbox is enabled
+     *  - the "LOG IN" button is enabled
      *
      * Notes:
-     *  -
-     *
-     * Hint(s):
-     *  -
+     *  - the "Email Input" view has ID: emailEditText (R.id.emailEditText)
+     *  - the "Password Input" view has ID: passwordEditText (R.id.passwordEditText)
+     *  - the "I agree" checkbox has ID: agreeCheckBox (R.id.agreeCheckBox)
+     *  - the "LOG IN" button has ID: loginButton (R.id.loginButton)
      */
     @Test
     fun shouldBeEnabledAllViews() {
@@ -36,13 +39,14 @@ class LoginActivityTest {
 
     /**
      * Test case:
-     *  -
+     *  - the "Email Input" has the "Email" hint
+     *  - the "Password Input" has the "Password" hint
      *
      * Notes:
-     *  -
-     *
-     * Hint(s):
-     *  -
+     *  - the "Email Input" view has ID: emailEditText (R.id.emailEditText)
+     *  - the "Password Input" view has ID: passwordEditText (R.id.passwordEditText)
+     *  - the "Email" string can be found in strings.xml file as hint_email const (R.string.hint_email)
+     *  - the "Password" string can be found in strings.xml file as hint_password const (R.string.hint_password)
      */
     @Test
     fun shouldEmailAndPasswordFieldsDisplayHint() {
@@ -51,13 +55,23 @@ class LoginActivityTest {
 
     /**
      * Test case:
-     *  -
+     *  - the "Email Input" is blank
+     *  - the "Password Input" is blank
+     *  - the error message with "Email and password are blank" text should be displayed
+     *  after click on "LOG IN" button
      *
      * Notes:
-     *  -
+     *  - the "Email Input" view has ID: emailEditText (R.id.emailEditText)
+     *  - the "Password Input" view has ID: passwordEditText (R.id.passwordEditText)
+     *  - the "LOG IN" button has ID: loginButton (R.id.loginButton)
+     *  - the "Email and password are blank" string can be found in strings.xml file as
+     *  email_and_password_are_blank const (R.string.email_and_password_are_blank)
+     *  - the Snackbar view has error message
+     *  - the Snackbar text message view has ID: snackbar_text
+     *  (com.google.android.material.R.id.snackbar_text)
      *
      * Hint(s):
-     *  -
+     *  - error text can be found by text on screen or by text in view
      */
     @Test
     fun shouldDisplayErrorWhenEmailAndPasswordAreBlank() {
@@ -66,13 +80,23 @@ class LoginActivityTest {
 
     /**
      * Test case:
-     *  -
+     *  - the "Email Input" is blank
+     *  - the "Password Input" is NOT blank
+     *  - the error message with "Email is blank" text should be displayed
+     *  after click on "LOG IN" button
      *
      * Notes:
-     *  -
+     *  - the "Email Input" view has ID: emailEditText (R.id.emailEditText)
+     *  - the "Password Input" view has ID: passwordEditText (R.id.passwordEditText)
+     *  - the "LOG IN" button has ID: loginButton (R.id.loginButton)
+     *  - the "Email is blank" string can be found in strings.xml file as
+     *  email_is_blank const (R.string.email_is_blank)
+     *  - the Snackbar view has error message
+     *  - the Snackbar text message view has ID: snackbar_text
+     *  (com.google.android.material.R.id.snackbar_text)
      *
      * Hint(s):
-     *  -
+     *  - error text can be found by text on screen or by text in view
      */
     @Test
     fun shouldDisplayErrorWhenEmailIsBlank() {
@@ -81,13 +105,23 @@ class LoginActivityTest {
 
     /**
      * Test case:
-     *  -
+     *  - the "Email Input" is NOT blank
+     *  - the "Password Input" is blank
+     *  - the error message with "Password is blank" text should be displayed
+     *  after click on "LOG IN" button
      *
      * Notes:
-     *  -
+     *  - the "Email Input" view has ID: emailEditText (R.id.emailEditText)
+     *  - the "Password Input" view has ID: passwordEditText (R.id.passwordEditText)
+     *  - the "LOG IN" button has ID: loginButton (R.id.loginButton)
+     *  - the "Password is blank" string can be found in strings.xml file as
+     *  password_is_blank const (R.string.password_is_blank)
+     *  - the Snackbar view has error message
+     *  - the Snackbar text message view has ID: snackbar_text
+     *  (com.google.android.material.R.id.snackbar_text)
      *
      * Hint(s):
-     *  -
+     *  - error text can be found by text on screen or by text in view
      */
     @Test
     fun shouldDisplayErrorWhenPasswordIsBlank() {
@@ -96,13 +130,23 @@ class LoginActivityTest {
 
     /**
      * Test case:
-     *  -
+     *  - the "Email Input" doesn't match with email template "TEXT@TEXT.TEXT"
+     *  - the "Password Input" is NOT blank
+     *  - the error message with "Text is not email" text should be displayed
+     *  after click on "LOG IN" button
      *
      * Notes:
-     *  -
+     *  - the "Email Input" view has ID: emailEditText (R.id.emailEditText)
+     *  - the "Password Input" view has ID: passwordEditText (R.id.passwordEditText)
+     *  - the "LOG IN" button has ID: loginButton (R.id.loginButton)
+     *  - the "Text is not email" string can be found in strings.xml file as
+     *  text_is_not_email const (R.string.text_is_not_email)
+     *  - the Snackbar view has error message
+     *  - the Snackbar text message view has ID: snackbar_text
+     *  (com.google.android.material.R.id.snackbar_text)
      *
      * Hint(s):
-     *  -
+     *  - error text can be found by text on screen or by text in view
      */
     @Test
     fun shouldDisplayErrorWhenEmailTextIsNotEmail() {
@@ -111,13 +155,25 @@ class LoginActivityTest {
 
     /**
      * Test case:
-     *  -
+     *  - the "Email Input" has text which not matches with
+     *  [com.alexzh.testapp.data.DummyData.LOGIN] const
+     *  - the "Password Input" has text which not matches with
+     *  [com.alexzh.testapp.data.DummyData.PASSWORD] const
+     *  - the error message with "Check email and password" text should be displayed
+     *  after click on "LOG IN" button
      *
      * Notes:
-     *  -
+     *  - the "Email Input" view has ID: emailEditText (R.id.emailEditText)
+     *  - the "Password Input" view has ID: passwordEditText (R.id.passwordEditText)
+     *  - the "LOG IN" button has ID: loginButton (R.id.loginButton)
+     *  - the "Check email and password" string can be found in strings.xml file as
+     *  check_email_and_password const (R.string.check_email_and_password)
+     *  - the Snackbar view has error message
+     *  - the Snackbar text message view has ID: snackbar_text
+     *  (com.google.android.material.R.id.snackbar_text)
      *
      * Hint(s):
-     *  -
+     *  - error text can be found by text on screen or by text in view
      */
     @Test
     fun shouldDisplayErrorWhenEmailAndPasswordAreIncorrect() {
@@ -126,13 +182,25 @@ class LoginActivityTest {
 
     /**
      * Test case:
-     *  -
+     *  - the "Email Input" has text which not matches with
+     *  [com.alexzh.testapp.data.DummyData.LOGIN] const
+     *  - the "Password Input" has text which matches with
+     *  [com.alexzh.testapp.data.DummyData.PASSWORD] const
+     *  - the error message with "Check email and password" text should be displayed
+     *  after click on "LOG IN" button
      *
      * Notes:
-     *  -
+     *  - the "Email Input" view has ID: emailEditText (R.id.emailEditText)
+     *  - the "Password Input" view has ID: passwordEditText (R.id.passwordEditText)
+     *  - the "LOG IN" button has ID: loginButton (R.id.loginButton)
+     *  - the "Check email and password" string can be found in strings.xml file as
+     *  check_email_and_password const (R.string.check_email_and_password)
+     *  - the Snackbar view has error message
+     *  - the Snackbar text message view has ID: snackbar_text
+     *  (com.google.android.material.R.id.snackbar_text)
      *
      * Hint(s):
-     *  -
+     *  - error text can be found by text on screen or by text in view
      */
     @Test
     fun shouldDisplayErrorWhenEmailIsIncorrect() {
@@ -141,13 +209,25 @@ class LoginActivityTest {
 
     /**
      * Test case:
-     *  -
+     *  - the "Email Input" has text which  matches with
+     *  [com.alexzh.testapp.data.DummyData.LOGIN] const
+     *  - the "Password Input" has text which not matches with
+     *  [com.alexzh.testapp.data.DummyData.PASSWORD] const
+     *  - the error message with "Check email and password" text should be displayed
+     *  after click on "LOG IN" button
      *
      * Notes:
-     *  -
+     *  - the "Email Input" view has ID: emailEditText (R.id.emailEditText)
+     *  - the "Password Input" view has ID: passwordEditText (R.id.passwordEditText)
+     *  - the "LOG IN" button has ID: loginButton (R.id.loginButton)
+     *  - the "Check email and password" string can be found in strings.xml file as
+     *  check_email_and_password const (R.string.check_email_and_password)
+     *  - the Snackbar view has error message
+     *  - the Snackbar text message view has ID: snackbar_text
+     *  (com.google.android.material.R.id.snackbar_text)
      *
      * Hint(s):
-     *  -
+     *  - error text can be found by text on screen or by text in view
      */
     @Test
     fun shouldDisplayErrorWhenPasswordIsIncorrect() {
@@ -156,31 +236,31 @@ class LoginActivityTest {
 
     /**
      * Test case:
-     *  -
+     *  - the "Email Input" has text which matches with
+     *  [com.alexzh.testapp.data.DummyData.LOGIN] const
+     *  - the "Password Input" has text which matches with
+     *  [com.alexzh.testapp.data.DummyData.PASSWORD] const
+     *  - the "I agree" checkbox is checked
+     *  - the error message with "Check email and password" text should be displayed
+     *  after click on "LOG IN" button
      *
      * Notes:
-     *  -
+     *  - the "Email Input" view has ID: emailEditText (R.id.emailEditText)
+     *  - the "Password Input" view has ID: passwordEditText (R.id.passwordEditText)
+     *  - the "LOG IN" button has ID: loginButton (R.id.loginButton)
+     *  - the "I agree" checkbox has ID: i_agree (R.id.i_agree)
+     *  - the "I agree" string can be found in strings.xml file as i_agree const (R.string.i_agree)
+     *  - the "Check email and password" string can be found in strings.xml file as
+     *  check_email_and_password const (R.string.check_email_and_password)
+     *  - the Snackbar view has error message
+     *  - the Snackbar text message view has ID: snackbar_text
+     *  (com.google.android.material.R.id.snackbar_text)
      *
      * Hint(s):
-     *  -
+     *  - error text can be found by text on screen or by text in view
      */
     @Test
-    fun shouldDisplayErrorWhenAgreeCheckboxIsNotChecked() {
-        fail()
-    }
-
-    /**
-     * Test case:
-     *  -
-     *
-     * Notes:
-     *  -
-     *
-     * Hint(s):
-     *  -
-     */
-    @Test
-    fun shouldOpenHomeScreenWhenEmailAndPasswordAreCorrect() {
+    fun shouldOpenHomeScreenWhenEmailAndPasswordAreCorrectAndAgreeCheckboxIsChecked() {
         fail()
     }
 }
